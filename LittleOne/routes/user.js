4a724daace16a70244efc5327cf8a898cfcf2596
deepika-0507 <deepikasowmya.5@gmail.com>
2020-04-03 21:03:14 +0530

    Articles display

--- LittleOne/routes/user.js
+++ LittleOne/routes/user.js
@@ -7,6 +7,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const nodemailer = require('nodemailer');
 
 const Users=require('../models/Users');
+const Articles=require('../models/Articles');
 
 router.get('/home',function(req,res){
 console.log(req.user);
@@ -14,12 +15,50 @@ console.log(req.user);
 });
 
 router.get('/pregnancy_home',function(req,res){
-    res.render('pregnancy_home');
+  console.log(req.user);
+    res.render('pregnancy_home',{user:req.user});
   });
 
 
-router.get('/counselors',function(req,res){
-    res.render('counselors');
+  router.get('/babycare_home',function(req,res){
+    res.render('babycare_home');
+  });
+
+
+
+
+  router.get('/counselors',function(req,res){
+    console.log(req.user);
+    res.render('counselors',{user:req.user});
+  });
+
+
+
+router.get('/counselors/articles',function(req,res){
+    Articles.find({},function(err,articles){
+      
+     
+      res.render('counsellors1',{articles:articles});
+
+      
+    })
+    
+  });
+
+  
+
+
+
+  router.get('/single_article/:id',function(req,res){
+    Articles.find({_id:req.params.id},function(err,details){
+      console.log(details[0].name)
+      var text = details[0].text
+      someText = text.replace(/\n/g, "");
+      console.log(someText)
+      res.render('single_article',{details:details});
+
+    })
+    
   });
 
 
